7e41f356f1b4f3da8dd9e0084e44ac4eecc7700e
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-19 21:53:41 +0000

    Tue, Mar 19, 2024, 10:53 PM +01:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,16 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    string height_in_str;
     int height;
     do
     {
-        height = get_int("Height: ");
-        if (height <= 0 )
+        height_in_str = get_string("Height: ");
+        if ( strlen(height_in_str) == 0)
         {
             break;
         }
+        height = atoi(height_in_str);
     }
     while (height>=9 || height<0);
 
