fc7f6bda6978d122a729c464a09d00d83b835ad8
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-19 23:07:37 +0000

    Wed, Mar 20, 2024, 12:07 AM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-char letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-int weights[26] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+char letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+int weights[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int calculateScore(string s);
 int getIndex(char c);
@@ -12,16 +13,14 @@ int getIndex(char c);
 int main()
 {
 
-
     string player1Word = get_string("Player 1: ");
     string player2Word = get_string("Player 2: ");
 
-
-    int  player1Score;
+    int player1Score;
     int player2Score;
 
     player1Score = calculateScore(player1Word);
-    player2Score =  calculateScore(player2Word);
+    player2Score = calculateScore(player2Word);
 
     if (player1Score == player2Score)
     {
@@ -33,21 +32,20 @@ int main()
     }
     else if (player1Score < player2Score)
     {
-        printf("Player 1 wins!");
+        printf("Player 2 wins!");
     }
 }
-
 
 int calculateScore(string s)
 {
     int score = 0;
-    for(int i = 0; i < strlen(s); i++) {
+    for (int i = 0; i < strlen(s); i++)
+    {
         char currentLetter = s[i];
         score += weights[getIndex(currentLetter)];
     }
     return score;
 }
-
 
 int getIndex(char c)
 {
