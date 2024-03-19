2c622301f665671bf91d63ae1770c7b8c308ad2e
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-19 22:18:14 +0000

    Tue, Mar 19, 2024, 11:18 PM +01:00

---
+++ cash/cash.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int change = get_int("Change owed: ");
+    int no_of_coins = 0;
+
+    while (change > 0)
+    {
+        if (change <= 25)
+    {
+        change = change - 25;
+        no_of_coins++;
+    }
+
+    else if (change <= 10)
+    {
+        change = change - 10;
+        no_of_coins++;
+    }
+
+    else if (change <= 5)
+    {
+        change = change - 5;
+        no_of_coins++;
+    }
+
+    else if (change <= 1)
+        {
+            change = change - 1;
+            no_of_coins++;
+        }
+    }
+
+    printf("%i", no_of_coins);
+
+}
