2b352522c22ba0844c35e6b8eb6e3967b7941cba
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-19 22:42:26 +0000

    Tue, Mar 19, 2024, 11:42 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,9 +1,41 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+char letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+int weights[26] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+
+int calculateScore(string s);
+int getIndex(char c, char letters[]);
 
 int main()
 {
-    int letters[26] = {
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+
+
+    string player1Word = get_string("Player 1: ");
+    string player2Word = get_string("Player 2: ");
+
+
+    int  player1Score;
+    int player2Score;
+
+    calculateScore(player1Word);
+
+
+}
+
+
+int calculateScore(string s)
+{
+    for(int i = 0; i < strlen(s); i++) {
+        char currentLetter = s[i];
+        printf("%c", currentLetter);
     }
+    return 0;
+}
+
+
+int getIndex(char c, char letters[])
+{
+    
 }
