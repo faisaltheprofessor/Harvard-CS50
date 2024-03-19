54d5d3d1a3a4cb046bbf2f2374d9b0b1505b7245
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-19 22:34:17 +0000

    Tue, Mar 19, 2024, 11:34 PM +01:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int letters[26] = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    }
+}
