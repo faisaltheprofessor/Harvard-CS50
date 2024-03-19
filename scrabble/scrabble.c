8ab1322fa8d02a8dfde31152398464e01e8835b5
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-19 22:59:06 +0000

    Tue, Mar 19, 2024, 11:59 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 char letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 int weights[26] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
@@ -12,26 +13,28 @@ int main()
 {
 
 
-    string player1Word = toUpper(get_string("Player 1: "));
+    string player1Word = get_string("Player 1: ");
     string player2Word = get_string("Player 2: ");
 
 
     int  player1Score;
     int player2Score;
 
-    calculateScore(player1Word);
+    player1Score = calculateScore(player1Word);
+    player2Score =  calculateScore(player2Word);
 
-
+    printf("Player 1 score: %i \nPlayer 2 score: %i", player1Score, player2Score);
 }
 
 
 int calculateScore(string s)
 {
+    int score = 0;
     for(int i = 0; i < strlen(s); i++) {
         char currentLetter = s[i];
-        printf("%i", getIndex(currentLetter));
+        score += weights[getIndex(currentLetter)];
     }
-    return 0;
+    return score;
 }
 
 
@@ -41,8 +44,7 @@ int getIndex(char c)
 
     for (int i = 0, length = sizeof(letters) / sizeof(letters[0]); i < length; i++)
     {
-        printf("character is %c and the letter checked again is %c", c, letters[i])
-        if (c == letters[i])
+        if (toupper(c) == letters[i])
         {
             index = i;
             break;
