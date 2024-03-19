acb8fc2ca0f5cc87f81bd116ba850745bbac8a53
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-19 21:46:01 +0000

    Tue, Mar 19, 2024, 10:46 PM +01:00

---
+++ mario-more/mario.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height>=9 || height<0);
+
+    for (int i = 1; i<=height; i++)
+    {
+        printf("#");
+    }
+    return 0;
+}
