ac021729ad9716d132383a4958a423340cb08b3f
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-19 21:40:35 +0000

    Tue, Mar 19, 2024, 10:40 PM +01:00

--- me/hello.c
+++ me/hello.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
     string name = get_string("Enter your name: ");
     printf("hello, %s\n", name);
     return 0;
