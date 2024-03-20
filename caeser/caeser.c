b664aa91119d7009c3074500c88083db51b5a3b1
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-20 00:28:44 +0000

    Wed, Mar 20, 2024, 1:28 AM +01:00

---
+++ caeser/caeser.c
@@ -0,0 +1,45 @@
+#include "cs50.h"
+#include "stdio.h"
+#include "string.h"
+#include "ctype.h"
+
+int main(int argc, string argv[])
+{
+    // Handling lack of arguments
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    int k = atoi(argv[1]);
+    // Prompting the user for text
+    string prompt = GetString();
+
+    for (int i = 0; i < strlen(prompt);i++)
+    {
+    // In case of alphabets
+        if (isalpha(prompt[i]))
+        {
+            // In case of upper case letters
+            if (islower(prompt[i]))
+            {
+                printf("%c", ((((prompt[i] - 97) + k) % 26) + 97));
+            }
+
+            else
+            {
+                // In case of lower case letters
+                printf("%c", ((((prompt[i] - 65) + k) % 26) + 65));
+            }
+        }
+
+        else
+        {
+            // In case of non-alphabets
+            printf("%c", prompt[i]);
+        }
+
+    }
+    // Printing a new line after the loop execution has finished
+    printf("\n");
+}
