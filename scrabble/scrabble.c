2692cb17557adadb587950f8369491da2b7da265
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-20 22:29:21 +0000

    Wed, Mar 20, 2024, 11:29 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,9 +3,23 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    char character;
+    int weight;
+}
+letter;
+
+letter letters[26] = {
+    new letter
+}
+
 char letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 int weights[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+
+
 
 int calculateScore(string s);
 int getIndex(char c);
