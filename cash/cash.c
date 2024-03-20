3dcbbb34dea4ead66f49280897770483e0f679b0
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-20 21:21:36 +0000

    Wed, Mar 20, 2024, 10:21 PM +01:00

--- cash/cash.c
+++ cash/cash.c
@@ -6,7 +6,7 @@ int main(void)
 {
     int change;
     int no_of_coins = 0;
-    string coins = NULL;
+
     do
     {
         change = get_int("Change owed: ");
@@ -19,28 +19,28 @@ int main(void)
         {
             change = change - 25;
             no_of_coins++;
-            strcat(coins, " 25 ");
+            printf("%i\n", 25);
         }
 
         else if (change >= 10)
         {
             change = change - 10;
             no_of_coins++;
-             strcat(coins, " 10 ");
+             printf("%i\n", 10);
         }
 
         else if (change >= 5)
         {
             change = change - 5;
             no_of_coins++;
-             strcat(coins, " 5 ");
+             printf("%i\n", 5);
         }
 
         else if (change >= 1)
         {
             change = change - 1;
             no_of_coins++;
-            strcat(coins, " 1 ");
+            printf("%i\n", 1);
         }
     }
 
