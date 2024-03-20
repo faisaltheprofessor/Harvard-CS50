9ad9a5d4eea0a914eb930da15b705f3fbd0506ec
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-20 00:43:38 +0000

    Wed, Mar 20, 2024, 1:43 AM +01:00

--- caeser/caesar.c
+++ caeser/caesar.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
-#include "string.h"
-#include "ctype.h"
-#include "stdlib.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
@@ -12,33 +12,32 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    int k = atoi(argv[1]);
+
+    int k = atoi(argv[1]) % 26; // Ensure key is within the range of letters
+
     // Prompting the user for text
     string prompt = get_string("Enter Text: ");
 
-    for (int i = 0; i < strlen(prompt);i++)
+    for (int i = 0; i < strlen(prompt); i++)
     {
-    // In case of alphabets
+        // In case of alphabets
         if (isalpha(prompt[i]))
         {
             // In case of upper case letters
             if (islower(prompt[i]))
             {
-                printf("%c", ((((prompt[i] - 97) + k) % 26) + 97));
+                printf("%c", ((((prompt[i] - 'a') + k) % 26) + 'a');
             }
-
             else
             {
                 // In case of lower case letters
-                printf("%c", ((((prompt[i] - 65) + k) % 26) + 65));
+                printf("%c", ((((prompt[i] - 'A') + k) % 26) + 'A');
             }
         }
-
         else
         {
             // In case of non-alphabets
             printf("%c", prompt[i]);
         }
-
     }
 }
