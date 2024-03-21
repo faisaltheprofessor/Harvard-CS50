f526ceede0c48dee3a1509a8d46df34bbf94c5ce
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-21 21:45:28 +0000

    Thu, Mar 21, 2024, 10:45 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -68,15 +68,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-             RGBTRIPLE pixel = image[i][j];
-
-            int rgbtRed = pixel.rgbtRed;
-            int rgbtGreen = pixel.rgbtGreen;
-            int rgbtBlue = pixel.rgbtBlue;
-
-            image[i][j].rgbtRed = rgbtBlue;
-            // image[i][j].rgbtGreen = sepiaGreen;
-            image[i][j].rgbtBlue = rgbtRed;
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[j][i];
+            image[j][i] = temp;
         }
     }
     return;
