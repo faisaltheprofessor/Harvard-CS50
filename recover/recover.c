6ece202227a984a215eebd7fc70cc26811d57e7c
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-27 23:24:31 +0000

    Thu, Mar 28, 2024, 12:24 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BLOCK_SIZE 512
+#define
+
 int main(int argc, char *argv[])
 {
 
@@ -11,12 +14,27 @@ int main(int argc, char *argv[])
     }
 
      // Open the memory card
-    FILE *card = fopen(argv[1], "r");
+    FILE *memory_card = fopen(argv[1], "r");
+
+    if (memory_card == NULL)
+    {
+        printf("Could not open file \n");
+        return 2;
+    }
+
+    uint8_t buffer[BLOCK_SIZE];
+
+    int no_of_images_found = 0;
+    FILE * image = NULL;
+    bool found = false;
+
+
 
 
      // While there's still data left to read from the memory card
-
+     while(fread(buffer, sizeof(buffer), BLOCK_SIZE, memory_card) == BLOCK_SIZE)
         // Create JPEGs from the data
+        if(buffer[0] == 0xFF)
 
 
 }
