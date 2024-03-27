a22d8c6040b92f462ae8a6f043414e1e41d98e58
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-27 23:13:22 +0000

    Thu, Mar 28, 2024, 12:13 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,9 +6,17 @@ int main(int argc, char *argv[])
 
     if(argc != 2)
     {
-        printf("missing argument\n");
+        printf("Usage: ./recover FILE\n");
         return 1;
     }
 
-    
+     // Open the memory card
+    FILE *card = fopen(argv[1], "r");
+
+
+     // While there's still data left to read from the memory card
+
+        // Create JPEGs from the data
+
+
 }
