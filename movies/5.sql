29f89d14d853a5cc50115a8d09249be2e07264c7
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-28 22:45:36 +0000

    Thu, Mar 28, 2024, 11:45 PM +01:00

--- movies/5.sql
+++ movies/5.sql
@@ -2,4 +2,4 @@
 -- Your query should output a table with two columns, one for the title of each movie and one for the release year of each movie.
 -- You may assume that the title of all Harry Potter movies will begin with the words “Harry Potter”, and that if a movie title begins with the words “Harry Potter”, it is a Harry Potter movie.
 
-se
+SELECT title, year FROM movies WHERE title LIKE 'Harry Potter%';
