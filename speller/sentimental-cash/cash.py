a50daed82669598feb08536a394509446b37a36c
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-28 20:54:35 +0000

    Thu, Mar 28, 2024, 9:54 PM +01:00

--- speller/sentimental-cash/cash.py
+++ speller/sentimental-cash/cash.py
@@ -1,5 +1,28 @@
 from cs50 import get_float
 
-change = input("Change: ")
+while True:
+    change = get_float("Change: ")
+    if(change > 0):
+        break
 
+noOfCoins = 0
 
+changeInCents = change * 100
+while changeInCents != 0:
+    if changeInCents >= 25:
+        changeInCents = changeInCents - 25
+        noOfCoins = noOfCoins + 1
+
+    elif changeInCents >= 10:
+        changeInCents = changeInCents - 10
+        noOfCoins = noOfCoins + 1
+
+    elif changeInCents >= 5:
+            changeInCents = changeInCents - 5
+            noOfCoins = noOfCoins + 1
+
+    elif changeInCents >= 1:
+                changeInCents = changeInCents - 1
+                noOfCoins = noOfCoins + 1
+
+print(noOfCoins)
