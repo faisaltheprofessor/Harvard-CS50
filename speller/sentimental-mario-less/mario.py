eaeaae3f95791f157707831802eaf4f6e66428f0
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-28 20:42:56 +0000

    Thu, Mar 28, 2024, 9:42 PM +01:00

--- speller/sentimental-mario-less/mario.py
+++ speller/sentimental-mario-less/mario.py
@@ -3,9 +3,9 @@ from cs50 import get_int
 
 while True:
     height = get_int("Height:")
-    if(height <=8 and height > 0):
+    if height <= 8 and height > 0:
         break
 
 
 for i in range(1, height + 1):
-    print (" " * (height - i) + ("#" * i) )
+    print(" " * (height - i) + ("#" * i))
