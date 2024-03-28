1e37aff0b29afc0334cdf3a5c1a67795fca92390
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-28 22:50:07 +0000

    Thu, Mar 28, 2024, 11:50 PM +01:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,3 +1,3 @@
 -- In 7.sql, write a SQL query to list all movies released in 2010 and their ratings, in descending order by rating. For movies with the same rating, order them alphabetically by title.
 
-
+ SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE YEAR = 2010 and rating IS NOT NULL ORDER BY rating, title;
