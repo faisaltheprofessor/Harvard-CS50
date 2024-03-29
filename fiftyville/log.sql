8613078febda57c8fcdeb08849a37f7694b8f780
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-29 20:49:45 +0000

    Fri, Mar 29, 2024, 9:49 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,6 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- Listing the description of the crimes scencs took place on jly 28
+-- Listing the description of the crimes scencs took place on july 28
 select description, street  from crime_scene_reports where day = 28 and month = 7 and street = 'Humphrey Street';
 
 -- Since the description says each of interview transcripts mentions the bakery, chechking the interviews table transcript and look into the ones mentioning the bakery
