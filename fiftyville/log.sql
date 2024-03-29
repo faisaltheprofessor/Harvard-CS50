ad4717ea0019a6f1f0aad3ec2cb5d1f8bac8e829
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-29 20:38:51 +0000

    Fri, Mar 29, 2024, 9:38 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,6 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Listing the description of the crimes scencs took place on jly 28
-select description from crime_scene_reports where day = 28 and month = 7;
+select description, street  from crime_scene_reports where day = 28 and month = 7;
 
 
