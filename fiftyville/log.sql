98143daddbbda0ef2ce719fa6f914eb23f71403e
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-29 20:41:20 +0000

    Fri, Mar 29, 2024, 9:41 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,6 +3,7 @@
 -- Listing the description of the crimes scencs took place on jly 28
 select description, street  from crime_scene_reports where day = 28 and month = 7 and street = 'Humphrey Street';
 
--- 
+-- Since the description says each of interview transcripts mentions the bakery, chechking the bakery_security_logs
+
 
 
