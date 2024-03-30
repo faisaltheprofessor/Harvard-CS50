9d72c58ad6393f01848c3c5f193ce1e0675cd9c0
Faisal Khan <94072607+faisalfazily@users.noreply.github.com>
2024-03-30 22:22:48 +0000

    Sat, Mar 30, 2024, 11:22 PM +01:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -28,8 +28,14 @@ def index():
 
         # TODO: Add the user's entry into the database
         name = request.form.get('name')
+        if not name:
+            return redirect("/")
         month = request.form.get('month')
+        if not month:
+            return redirect("/")
         day = request.form.get('day')
+        if not day:
+            return redirect("/")
 
         db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
@@ -38,6 +44,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM birthdays")
 
         return render_template("index.html")
 
